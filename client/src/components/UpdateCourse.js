47497aaf8ccd1d2f3d9a51ba294421240fbca7a2
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-01 23:43:07 -0500

    add course to update course fields

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,6 +1,48 @@
 import React,{Component} from 'react';
 
 class UpdateCourse extends Component {
+
+    state = {
+        course: {
+            title: null,
+            description: '',
+            estimatedTime: null,
+            materialsNeeded: '',
+            user: {
+                firstName: null,
+                lastName: null
+            }
+        }
+    }
+    
+    handleDescChange(e){
+        this.setState({
+            course:{...this.state.course, description:e.target.value}
+        })
+    }
+    
+    handleMatsChange(e){
+        this.setState({
+            course:{...this.state.course, materialsNeeded:e.target.value}
+        })
+    }
+
+    cancel(e){
+        e.preventDefault();
+        this.props.history.push(`/courses/${this.props.id}`);
+    }
+
+    componentDidMount(){
+        fetch(`http://localhost:5000/api/courses/${this.props.id}`)
+            .then(res => {
+                res.json()
+                    .then(course =>{
+                        if(res.status === 200)
+                        this.setState({course})
+                    })
+            })
+    }
+
     render(){
         return(
             <div className="bounds course--detail">
@@ -11,11 +53,11 @@ class UpdateCourse extends Component {
                     <div className="course--header">
                         <h4 className="course--label">Course</h4>
                         <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                            defaultValue="Build a Basic Bookcase" /></div>
-                        <p>By Joe Smith</p>
+                            defaultValue={this.state.course.title} /></div>
+                        <p>By {this.state.course.user.firstName} {this.state.course.user.lastName}</p>
                     </div>
                     <div className="course--description">
-                        <div><textarea id="description" name="description" className="" placeholder="Course description..."></textarea></div>
+                        <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={(e) => this.handleDescChange(e)} value={this.state.course.description}/></div>
                     </div>
                     </div>
                     <div className="grid-25 grid-right">
@@ -24,16 +66,19 @@ class UpdateCourse extends Component {
                         <li className="course--stats--list--item">
                             <h4>Estimated Time</h4>
                             <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                                placeholder="Hours" defaultValue="14 hours" /></div>
+                                placeholder="Hours" defaultValue={this.state.course.estimatedTime} /></div>
                         </li>
                         <li className="course--stats--list--item">
                             <h4>Materials Needed</h4>
-                            <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..."></textarea></div>
+                            <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={(e) => this.handleMatsChange(e)} value={this.state.course.materialsNeeded}/></div>
                         </li>
                         </ul>
                     </div>
                     </div>
-                    <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary">Cancel</button></div>
+                    <div className="grid-100 pad-bottom">
+                        <button className="button" type="submit">Update Course</button>
+                        <button className="button button-secondary" onClick={(e) => this.cancel(e)}>Cancel</button>
+                    </div>
                 </form>
                 </div>
             </div>
