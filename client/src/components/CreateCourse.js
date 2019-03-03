472a743c96b3f60baa51c227ebd970acb7cfea9e
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-03-03 18:52:42 -0500

    add create course functionality

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,21 +1,65 @@
 import React,{Component} from 'react';
 
 class CreateCourse extends Component {
+
+    state = {
+        messages: null
+    }
+
+    createCourse(e){
+        this.setState({messages:null});
+        e.preventDefault();
+        const data = JSON.stringify({
+            title: e.target[0].value,
+            description: e.target[1].value, 
+            estimatedTime: e.target[2].value,
+            materialsNeeded: e.target[3].value
+        });
+        fetch('http://localhost:5000/api/courses', {
+            method: "POST",
+            body: data,
+            headers: this.props.user.headers
+            }
+        )
+            .then(res => {
+                if(res.status !== 201){
+                    res.json()
+                        .then(res =>{
+                            let messages = res.message.split(","); //split the errors
+                            messages = messages.map(message => message.split(":")); //split titles from errors
+                            messages = messages.map(message => message.pop()); //take only the error
+                            this.setState({messages});
+                        });
+                }else{
+                    //redirect
+                    this.props.history.push('/');
+                }
+            });
+    }
+
+    printErrors(){
+        if(this.state.messages !== null){
+            const messages = this.state.messages.map((message, i) => <li key={i}>{message}</li>)
+            return(
+                <div>
+                    <h2 className="validation--errors--label">Validation errors</h2>
+                    <div className="validation-errors">
+                        <ul>
+                            {messages}
+                        </ul>
+                    </div>
+                </div>
+            );
+        }
+    }
+
     render(){
         return(
             <div className="bounds course--detail">
                 <h1>Create Course</h1>
                 <div>
-                    <div>
-                        <h2 className="validation--errors--label">Validation errors</h2>
-                        <div className="validation-errors">
-                        <ul>
-                            <li>Please provide a value for "Title"</li>
-                            <li>Please provide a value for "Description"</li>
-                        </ul>
-                        </div>
-                    </div>
-                    <form>
+                    {this.printErrors()}
+                    <form onSubmit={(e) => this.createCourse(e)}>
                         <div className="grid-66">
                         <div className="course--header">
                             <h4 className="course--label">Course</h4>
@@ -42,7 +86,10 @@ class CreateCourse extends Component {
                             </ul>
                         </div>
                         </div>
-                        <div className="grid-100 pad-bottom"><button className="button" type="submit">Create Course</button><button className="button button-secondary">Cancel</button></div>
+                        <div className="grid-100 pad-bottom">
+                            <button className="button" type="submit">Create Course</button>
+                            <button className="button button-secondary" onClick={(e)=> {e.preventDefault();this.props.history.push('/');}}>Cancel</button>
+                        </div>
                     </form>
                 </div>
             </div>
